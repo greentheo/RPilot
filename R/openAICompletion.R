6c3e2f394597d6b5ab313362d190df18450f538c
VanRooy <theov@amazon.com>
2023-01-02 18:55:14 -0700

    adding in autodependencies

--- R/openAICompletion.R
+++ R/openAICompletion.R
@@ -1,5 +1,5 @@
 openAICompletion = function(echo=T){
-  prompt = getSourceEditorContext()
+  prompt = rstudioapi::getSourceEditorContext()
   print(prompt$selection[[1]]$text)
   request = list(model = "text-davinci-003",
                  prompt = prompt$selection[[1]]$text,
@@ -8,13 +8,13 @@ openAICompletion = function(echo=T){
                  echo=echo)
 
 
-  response = POST(url = "https://api.openai.com/v1/completions",
-                  add_headers(`Content-Type` = "application/json",
-                              Authorization = "Bearer your-token-goes-here"),
-                  body = toJSON(request)
+  response = httr::POST(url = "https://api.openai.com/v1/completions",
+                  httr::add_headers(`Content-Type` = "application/json",
+                              Authorization = "Bearer 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"),
+                  body = RJSONIO::toJSON(request)
   )
   print(response)
-  print(content(response)$choices[[1]]$text)
+  print(httr::content(response)$choices[[1]]$text)
 
-  modifyRange(location =prompt$selection[[1]]$range, content(response)$choices[[1]]$text, id = prompt$id)
+  rstudioapi::modifyRange(location =prompt$selection[[1]]$range, httr::content(response)$choices[[1]]$text, id = prompt$id)
 }
